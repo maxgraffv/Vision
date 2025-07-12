8274f5b4ec4e71a065ccb157970b9701aa057e24
Max Graff <maksymilian.graff@gmail.com>
2025-07-12 13:09:56 +0200

    Defined and declared AppWindow

---
+++ AppWindow/AppWindow.cpp
@@ -0,0 +1,21 @@
+#include "AppWindow.h"
+
+#include <QPushButton>
+#include <QLabel>
+#include <QVBoxLayout>
+
+
+AppWindow::AppWindow(QWidget* parent) : QWidget(parent)
+{
+    _button = new QPushButton("Click", this);
+    _label = new QLabel("Hello Label", this);
+
+    auto* layout = new QVBoxLayout();
+    layout->addWidget(_label);
+    setLayout(layout);
+
+    connect(_button, &QPushButton::clicked, this, [=](){
+        _label->setText("Ugh pushed");
+    });
+
+}
---
+++ AppWindow/AppWindow.h
@@ -0,0 +1,27 @@
+#ifndef APP_WINDOW_H
+#define APP_WINDOW_H
+
+#include <QWidget>
+
+class QPushButton;
+class QLabel;
+
+class AppWindow : public QWidget
+{
+    Q_OBJECT
+
+    private:
+        QPushButton* _button;
+        QLabel* _label;
+
+
+    public:
+        AppWindow(QWidget *parent = nullptr);
+    
+};
+
+
+
+
+
+#endif
