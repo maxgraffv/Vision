2aaf89de0377eaa923d4413c38a57b6f6d029640
Max Graff <maksymilian.graff@gmail.com>
2025-07-14 22:53:38 +0200

    created QtImage class

---
+++ Image/QtImage.h
@@ -0,0 +1,28 @@
+#ifndef QT_IMAGE_H
+#define QT_IMAGE_H
+
+#include "Image.h"
+#include <QWidget>
+#include <QLabel>
+
+
+class QtImage : public Image, public QLabel
+{
+
+    private:
+        QImage* _qImage;
+        int _width, _height;
+
+        friend class AppWindow;
+
+
+    public:
+        QtImage(int width, int height, const char* path);
+
+
+
+};
+
+
+
+#endif
